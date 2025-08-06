c00af5dd9ae792dd28284b70e7f9415f4261fff2
liuyuanzhi-cn <2632285589@qq.com>
2025-08-06 18:21:01 +0800

    测试添加评论

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type {ReactNode} from 'react';
+import {useEffect, useRef, type ReactNode} from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -30,6 +30,27 @@ function HomepageHeader() {
 }
 
 export default function Home(): ReactNode {
+  const commentElement = useRef(null);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    let s = document.createElement("script");
+    s.src = "https://giscus.app/client.js";
+    s.setAttribute("data-repo", "liuyuanzhi-cn/blogs-comments");
+    s.setAttribute("data-repo-id", "R_kgDOPZQGmQ");
+    s.setAttribute("data-category", "General");
+    s.setAttribute("data-category-id", "DIC_kwDOPZQGmc4Ct18K");
+    s.setAttribute("data-mapping", "pathname");
+    s.setAttribute("data-reactions-enabled", "1");
+    s.setAttribute("data-emit-metadata", "0");
+    s.setAttribute("data-input-position", "bottom");
+    s.setAttribute("data-theme", "light");
+    s.setAttribute("data-lang", "zh-CN");
+    s.setAttribute("crossorigin", "anonymous");
+    s.async = true;
+    commentElement.current.appendChild(s);
+  }, []);
+  
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
@@ -38,6 +59,7 @@ export default function Home(): ReactNode {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <div style={{marginTop:'20px'}} ref={commentElement}></div>
       </main>
     </Layout>
   );
